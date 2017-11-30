696d20e1a5203948d0c7f3cb0c3aac93a2ea40de
Travis CI <travis@travis-ci.org>
2017-11-30 23:52:06 +0000

    v0.0.5

--- dist/aw-liner.amd.js
+++ dist/aw-liner.amd.js
@@ -7,7 +7,7 @@
 		exports["awLiner"] = factory();
 	else
 		root["awLiner"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
